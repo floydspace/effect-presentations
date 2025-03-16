430a259b51e3aa95b7d46b68a7cb8880aac822d2
Victor Korzunin <5180700+floydspace@users.noreply.github.com>
2025-03-16 09:55:07 +0100

    fix: use powertools-logger

--- effect-app/src/index.ts
+++ effect-app/src/index.ts
@@ -1,5 +1,6 @@
 import { makeLambda } from "@effect-aws/lambda";
-import { Layer, Logger } from "effect";
+import { Logger } from "@effect-aws/powertools-logger";
+import { Layer } from "effect";
 import { SNSEventBusLive } from "./bus";
 import { effectHandler } from "./lambda";
 import { YahooQuoteClientLive } from "./quoteClient";
@@ -9,7 +10,7 @@ const LambdaLive = Layer.mergeAll(
   SNSEventBusLive,
   YahooQuoteClientLive,
   MongoDbInstrumentStoreLive
-).pipe(Layer.provideMerge(Logger.structured));
+).pipe(Layer.provideMerge(Logger.layer({ serviceName: "effect-app" })));
 
 module.exports.handler = makeLambda({
   handler: effectHandler,
